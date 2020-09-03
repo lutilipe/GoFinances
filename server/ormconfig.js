f6dca3e18e324140bded857b92435a0b2f4e7afb
lutilipe <lutilipe02@hotmail.com>
2020-09-03 14:41:24 -0300

    Adding .env and .env.example files

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,10 +1,12 @@
-{
+require('dotenv').config()
+
+module.exports = {
   "type": "postgres",
-  "host": "localhost",
+  "host":  process.env.DB_HOST,
   "port": 5432,
-  "username": "postgres",
-  "password": "luiz081200",
-  "database": "gostack_desafio06",
+  "username": process.env.DB_USER,
+  "password":  process.env.DB_PASS,
+  "database":  process.env.DB_NAME,
   "entities": ["./src/models/*.ts"],
   "migrations": ["./src/database/migrations/*.ts"],
   "cli": {
